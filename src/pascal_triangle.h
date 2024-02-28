3c87a2d9170badb36a8ed44c6ea36ffe66c70fe1
Maksim <pumpbro@gmail.com>
2024-02-28 20:17:15 +0300

    clang

--- src/pascal_triangle.h
+++ src/pascal_triangle.h
@@ -7,7 +7,6 @@
 int calc_size(int lines);
 void clear(int *arr, int size);
 void calc_pascal(int lines, int *result);
-// int calc_result_size(int lines);
 void fill_result(int *triangle, int *result, int size, int triangle_size);
 void exception();
 void output_pascal(int *result, int size);
